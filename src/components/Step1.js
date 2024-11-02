56fc4b4580cffbe3245a783726f134102cc357ac
spia-dm <srisiddhaarthp@gmail.com>
2024-11-03 00:42:59 +0530

    test

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,8 +1,19 @@
+import {useState} from 'react'
 const Step1=()=>{
+    const [fn,set_fn] = useState('')
+    const [ln,set_ln] = useState('')
+
+    console.log(fn)
+    console.log(ln)
+
+    const export_name=()=>{
+        //function to function communication
+    }
+
     return(
         <div>
-           <input type="text" placeholder="First Name"></input> 
-           <input type="text" placeholder="Last Name"></input> 
+           <input type="text" onChange={(e)=>{set_fn(e.target.value)}} value={fn} placeholder="First Name"/> 
+           <input type="text" onChange={(e)=>{set_ln(e.target.value)}} value={ln} placeholder="Last Name"/> 
         </div>
     )
 }
